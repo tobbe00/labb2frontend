3323d5f98d18d5d9f4e98ee893579970c3783640
calle361 <calle361@gmail.com>
2024-11-18 10:49:58 +0100

    fixat så notes kan skapas

--- src/PatientJournal.js
+++ src/PatientJournal.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './PatientJournal.module.css';
-import patientJournal from "./PatientJournal"; // Importera CSS-modulen
+//import patientJournal from "./PatientJournal"; // Importera CSS-modulen
 
 function PatientJournal() {
     const { patientId } = useParams(); // Hämta patientens id från URL
